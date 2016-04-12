c94befd71582673c2c6480f773504a1cb8141ec9
hand9004 <hand9004@gmail.com>
2016-04-12 16:09:30 +0900

    - ConstantDefine => 고정적으로 사용할 값의 모음 클래스
    - CustomUtility
    => 게임 내에서 사용할 유틸리티 기능의 모음 클래스
          (확률 함수 추가)

---
+++ Assets/Script/CustomUtility.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace CustomUtility
+{
+    class UtilityFunctions
+    {
+        // 배열에 넣은 확률에 따라 어느 영역이 랜덤에 의해 선택됬는지 알려준다.
+        public static int GetWhereIsCorrect(int min, int max, float[] percentageDistributeArr)
+        {
+            // 배열 무결성 검사
+            float totalPercentage = 0.0f;
+            for (int i = 0; i < percentageDistributeArr.Length; ++i)
+            {
+                totalPercentage += percentageDistributeArr[i];
+            }
+            Debug.Assert(totalPercentage == 1.0f, "Total Percentage Should be 1.0f.");
+
+            int selectedArea = -1;
+            int pickedValue = UnityEngine.Random.Range(min, max);
+            int randomValueLength = max - min;
+
+            if (percentageDistributeArr.Length <= 2)
+            {
+                if (selectedArea <= min + (randomValueLength * percentageDistributeArr[0]))
+                {
+                    selectedArea = 0;
+                }
+                else
+                {
+                    selectedArea = 1;
+                }
+            }
+            else
+            {
+                float prevPercentageCheckVal = 0.0f;
+                float percentageCheckVal = randomValueLength * percentageDistributeArr[0];
+                for (int i = 0; i < percentageDistributeArr.Length - 1; ++i)
+                {
+                    if (pickedValue < percentageCheckVal && pickedValue >= prevPercentageCheckVal)
+                    {
+                        selectedArea = i;
+                        break;
+                    }
+                    else
+                    {
+                        prevPercentageCheckVal = percentageCheckVal;
+                        percentageCheckVal += randomValueLength * percentageDistributeArr[i + 1];
+                    }
+                }
+            }
+
+            return selectedArea;
+        }
+    }
+}
